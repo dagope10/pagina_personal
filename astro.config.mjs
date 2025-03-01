98bc356ccc64909ad96e85563362ed0663f589f8
Dani <daniobey24@gmail.com>
2025-03-01 18:50:28 +0100

    Avance en pagina web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
-  output: 'static', // Asegura que el output sea estático para Netlify
-  site: 'https://danielwebapp.netlify.app', // Reemplaza con tu URL de Netlify
-  base: '/' // Asegura que las rutas sean relativas a la raíz
+  // Asegura que el output sea estático para Netlify
+  output: 'static',
+
+  // Reemplaza con tu URL de Netlify
+  site: 'https://danielwebapp.netlify.app',
+
+  // Asegura que las rutas sean relativas a la raíz
+  base: '/',
+
+  integrations: [tailwind()]
 });
